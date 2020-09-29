8621582186101fe411dda790b5d979fd042ad7c0
ArthurDav <lachat.arthur74@gmail.com>
2020-09-29 23:32:50 +0900

    registration controller created

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationController < Devise::RegistrationController
+    # manual Registration Controller < Devise gem 
+    # define params required for sign up and updating account 
+  private
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+  end
+end
