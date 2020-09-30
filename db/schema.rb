e8c3b6e31e412cb06e2a7ccfe4064b4d28d6de10
ArthurDav <lachat.arthur74@gmail.com>
2020-09-30 11:49:39 +0900

    relation between user and tweets, a user has many tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_143405) do
+ActiveRecord::Schema.define(version: 2020_09_30_023357) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_143405) do
     t.text "tweet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
